beca43b9bee0c170d7d1b31cc746e3b4792c0553
andrew <andrew@ipxe-server.example.org>
2017-01-10 16:36:05 +0800

    kubeadm stable release

--- prepare_ipxe_env.sh
+++ prepare_ipxe_env.sh
@@ -3,7 +3,80 @@
 set -e
 
 K8SVersion=1.5.1
-CoreOSInstallationVersion=1185.5.0
+CoreOSInstallationVersion=1235.5.0
+iPXE_Server_IP=192.168.56.90
+RouterIP=192.168.56.1
+ethX=eth1
+
+UpdateConf() {
+  Subnet=${iPXE_Server_IP%.*}.0
+  Netmask=255.255.255.0
+  Range1=${iPXE_Server_IP%.*}.181
+  Range2=${iPXE_Server_IP%.*}.190
+  Broadcast=${iPXE_Server_IP%.*}.255
+
+  cat > etc/dhcp/dhcpd.conf.tpl << EOF
+  ddns-update-style none;
+  option domain-name "example.org";
+  option domain-name-servers 8.8.8.8;
+  default-lease-time 600;
+  max-lease-time 7200;
+  log-facility local7;
+
+  subnet $Subnet netmask $Netmask {
+    range $Range1 $Range2;
+    option routers $RouterIP;
+    option broadcast-address $Broadcast;
+    next-server $iPXE_Server_IP;
+    filename = "pxelinux.0";
+  }
+
+  host station {
+        hardware ethernet MACAddress;
+        fixed-address ServerIPAddress;
+  }
+EOF
+
+  cat > /var/www/html/ipxe/boot.ipxe << EOF
+#!ipxe
+
+set base-url http://${iPXE_Server_IP}/images/coreos/amd64-usr/${CoreOSInstallationVersion}
+kernel \${base-url}/vmlinuz cloud-config-url=http://${iPXE_Server_IP}/cloud-configs/ipxe-cloud-config.yml coreos.autologin
+initrd \${base-url}/cpio.gz
+boot
+EOF
+
+  cp -f systemd-conf/* /etc/systemd/system
+  systemctl daemon-reload
+  for i in nfs-server nginx tftp; do
+      systemctl enable $i
+      systemctl restart $i
+  done
+
+  rsync -avz root/bin/ /root/bin/
+  rsync -avz var/www/ /var/www/
+  rsync -avz var/tftpboot/ /var/tftpboot/
+  rsync -avz etc/dhcp/ /etc/dhcp/
+
+  WebDir=/var/www/html
+  sed -i "s/ethX/$ethX/g" /etc/systemd/system/dhcp.service
+  sed -i "s/K8SVersion/$K8SVersion/g" ${WebDir}/bin/*
+  sed -i "s/iPXE_Server_IP/$iPXE_Server_IP/g" ${WebDir}/bin/*
+  sed -i "s/iPXE_Server_IP/$iPXE_Server_IP/g" ${WebDir}/cloud-configs/template/*
+  sed -i "s/iPXE_Server_IP/$iPXE_Server_IP/g" /var/tftpboot/pxelinux.cfg/by_mac.tpl
+  sed -i "s/RouterIP/$RouterIP/g" ${WebDir}/cloud-configs/template/*
+}
+
+if [ "$1" == "-s" ]; then
+    UpdateConf
+    exit 0
+fi
+
+# pull ipxe environment service images
+docker pull networkboot/dhcpd
+docker pull nginx
+docker pull cpuguy83/nfs-server
+docker pull pghalliday/tftp
 
 # Create necessary directories
 mkdir -p /var/www/html/{ipxe,k8s}
@@ -83,67 +156,4 @@ ls -1 /var/www/html/images/docker/ > /var/www/html/images/docker-list
 
 docker run --rm -v /var/www/html/soft:/tmp/bin gcr.io/google_containers/hyperkube-amd64:v${K8SVersion} /bin/sh -c "cp -f /hyperkube /tmp/bin"
 
-iPXE_Server_IP=$1
-RouterIP=$2
-Subnet=${iPXE_Server_IP%.*}.0
-Netmask=255.255.255.0
-Range1=${iPXE_Server_IP%.*}.181
-Range2=${iPXE_Server_IP%.*}.190
-Broadcast=${iPXE_Server_IP%.*}.255
-cat > etc/dhcp/dhcpd.conf.tpl << EOF
-ddns-update-style none;
-option domain-name "example.org";
-option domain-name-servers 8.8.8.8;
-default-lease-time 600;
-max-lease-time 7200;
-log-facility local7;
-
-subnet $Subnet netmask $Netmask {
-  range $Range1 $Range2;
-  option routers $RouterIP;
-  option broadcast-address $Broadcast;
-  next-server $iPXE_Server_IP;
-  filename = "pxelinux.0";
-}
-
-host station {
-      hardware ethernet MACAddress;
-      fixed-address ServerIPAddress;
-}
-EOF
-
-cat > /var/www/html/ipxe/boot.ipxe << EOF
-#!ipxe
-
-set base-url http://${iPXE_Server_IP}/images/coreos/amd64-usr/${CoreOSInstallationVersion}
-kernel \${base-url}/vmlinuz cloud-config-url=http://${iPXE_Server_IP}/cloud-configs/ipxe-cloud-config.yml coreos.autologin
-initrd \${base-url}/cpio.gz
-boot
-EOF
-
-docker pull networkboot/dhcpd
-docker pull nginx
-docker pull cpuguy83/nfs-server
-docker pull pghalliday/tftp
-
-cp -f systemd-conf/* /etc/systemd/system
-systemctl daemon-reload
-for i in nfs-server nginx tftp; do
-    systemctl enable $i
-    systemctl restart $i
-done
-
-rsync -avz root/bin/ /root/bin/
-rsync -avz var/www/ /var/www/
-rsync -avz var/tftpboot/ /var/tftpboot/
-rsync -avz etc/dhcp/ /etc/dhcp/
-
-ethX=$3
-WebDir=/var/www/html
-sed -i "s/ethX/$ethX/g" /etc/systemd/system/dhcp.service
-sed -i "s/K8SVersion/$K8SVersion/g" ${WebDir}/bin/*
-sed -i "s/iPXE_Server_IP/$iPXE_Server_IP/g" ${WebDir}/bin/*
-sed -i "s/iPXE_Server_IP/$iPXE_Server_IP/g" ${WebDir}/cloud-configs/template/*
-sed -i "s/iPXE_Server_IP/$iPXE_Server_IP/g" /var/tftpboot/pxelinux.cfg/by_mac.tpl
-sed -i "s/RouterIP/$RouterIP/g" ${WebDir}/cloud-configs/template/*
-
+UpdateConf
--- var/www/html/bin/coreos-installd.sh
+++ var/www/html/bin/coreos-installd.sh
@@ -9,13 +9,13 @@ mkdir -p /opt/bin /opt/cni/bin /etc/cni/net.d /root/images/docker
 
 # Download docker necessary images
 for TAR in `curl http://iPXE_Server_IP/images/docker-list`; do
-  wget -N -P /root/images/docker http://iPXE_Server_IP/images/docker/$TAR
+  wget -q -N -P /root/images/docker http://iPXE_Server_IP/images/docker/$TAR
   docker load -i /root/images/docker/$TAR
 done
 
 # Install python 2.7.10.12 on CoreOS
 cd /opt
-wget http://iPXE_Server_IP/soft/ActivePython-2.7.10.12-linux-x86_64.tar.gz
+wget -q http://iPXE_Server_IP/soft/ActivePython-2.7.10.12-linux-x86_64.tar.gz
 tar xzvf ActivePython-2.7.10.12-linux-x86_64.tar.gz
 cd ActivePython-2.7.10.12-linux-x86_64 && ./install.sh -I /opt/python-2.7.10.12/
 ln -sf /opt/python-2.7.10.12/bin/easy_install /opt/bin/easy_install
@@ -29,26 +29,29 @@ curl -o /opt/bin/tmux http://iPXE_Server_IP/soft/tmux
 chmod +x /opt/bin/tmux
 
 # Install kubeadm on CoreOS
-wget -N -P /opt/bin http://iPXE_Server_IP/soft/kubeadm
+wget -q -N -P /opt/bin http://iPXE_Server_IP/soft/kubeadm
 chmod +x /opt/bin/kubeadm
 
 # Install cni binary file
-wget -N -P /opt http://iPXE_Server_IP/soft/cni-amd64-07a8a28637e97b22eb8dfe710eeae1344f69d16e.tar.gz
+wget -q -N -P /opt http://iPXE_Server_IP/soft/cni-amd64-07a8a28637e97b22eb8dfe710eeae1344f69d16e.tar.gz
 tar xzvf /opt/cni-amd64-07a8a28637e97b22eb8dfe710eeae1344f69d16e.tar.gz -C /opt/cni
 
 # Install bootkube
-#wget -N -P /opt/bin http://iPXE_Server_IP/soft/bootkube
+#wget -q -N -P /opt/bin http://iPXE_Server_IP/soft/bootkube
 #chmod +x /opt/bin/bootkube
 
 # Fetch bootkube asset
-#wget -N -P /root http://iPXE_Server_IP/k8s/asset.tar
+#wget -q -N -P /root http://iPXE_Server_IP/k8s/asset.tar
+
+# Fetch rkt images
+#rkt fetch --insecure-options=all http://iPXE_Server_IP/images/rkt/hyperkube/vK8SVersion_coreos.0/hyperkube.aci
 
 # Install kubectl on CoreOS
 docker run --rm  -v /opt/bin:/tmp/bin gcr.io/google_containers/hyperkube-amd64:vK8SVersion /bin/sh -c "cp /hyperkube /tmp/bin" && ln -s /opt/bin/hyperkube /opt/bin/kubectl
 
 # Download bash completion
 mkdir -p /root/downloads
-wget -N -P /root/downloads http://iPXE_Server_IP/soft/bash-completion.tgz
+wget -q -N -P /root/downloads http://iPXE_Server_IP/soft/bash-completion.tgz
 tar zxvf /root/downloads/bash-completion.tgz -C /var
 
 # Touch file
