ca05f1b90b1aa4592934128c9ef38a7aff31294e
andrew <andrew@ipxe-server.example.org>
2017-01-26 10:23:28 +0800

    added registry-installd.service

--- var/www/html/bin/ipxe-cleand.sh
+++ var/www/html/bin/ipxe-cleand.sh
@@ -5,9 +5,9 @@ mount iPXE_Server_IP:/pxelinux.cfg /mnt/nfs
 mount /dev/sda6 /mnt/rootfs/boot
 echo set linux_append="net.ifnames=0 biosdevname=0" > /mnt/rootfs/boot/grub.cfg
 
-mac=`ifconfig | sed -n '/192.168/,+2p' | tail -n 1 | awk '{print $2}'`
-macd=01-`echo $mac | tr ':' '-'`
-
+# Remove PXE configuration
+mac=`cat /root/installation_nic_mac`
+macd=01-`echo ${mac,,} | tr ':' '-'`
 cd /mnt/nfs
 rm $macd
 
